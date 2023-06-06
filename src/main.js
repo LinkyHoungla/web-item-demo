a5f740ad1d2f1c43f0a9ba846e733354c30534f6
Houngla Linky <baisss2000@sina.com>
2023-06-06 20:44:30 +0800

    [登录页面]验证，路由守卫

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
 import Vue from 'vue'
 
+//样式导入
+import '@/assets/css/global.css'
+
+//字体导入
+import '@/assets/fonts/iconfont.css'
+
 //Vue-router 导入
 import router from '@/router'
 import App from './App.vue'
 
 //Element-ui 导入
 import ElementUI from 'element-ui'
-
-import './plugins/element.js'
+import '@/plugins/element.js'
 Vue.use(ElementUI)
+
+//axios 导入
+import axios from 'axios'
+//配置请求路径
+axios.defaults.baseURL = 'http://localhost:8080/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
