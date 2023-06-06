279be20be387ae51f0fd2569347f8fbcda7b4f65
Houngla Linky <baisss2000@sina.com>
2023-06-06 15:07:02 +0800

    [初始配置]Vue-router, Element-UI, Axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+
+//Vue-router 导入
+import router from '@/router'
 import App from './App.vue'
+
+//Element-ui 导入
+import ElementUI from 'element-ui'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
