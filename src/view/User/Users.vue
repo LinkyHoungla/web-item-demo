b2ea29af9ccc23c4877472f6d53571b4c207d07a
LinkyHoungla <baisss2000@sina.com>
2023-06-13 14:13:41 +0800

    管理员删除功能

--- src/view/User/Users.vue
+++ src/view/User/Users.vue
@@ -259,6 +259,26 @@ export default {
         this.$message.success("修改成功");
       });
     },
+    // 根据 id 删除用户
+    async removeAdmin(id) {
+      // 确认返回 confirm，取消返回 cancel
+      const result = await this.$confirm(
+        "此操作将永久删除该用户，是否继续",
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        }
+      ).catch((err) => err);
+
+      if (result !== "confirm") return this.$message.info("已取消删除");
+      const { data: res } = await this.$http.delete("admin/" + id);
+
+      if (res.status !== 200) return this.$message.error("删除失败");
+      this.$message.success("删除成功");
+      this.getAdminList();
+    },
   },
 };
 </script>
